de0fed113068d10b7080960e6c9d6818a9c0b447
gusclemente <gusclemente77@gmail.com>
2025-02-04 18:17:24 -0300

    Criação do repositório de estudos sobre Python

---
+++ cursoemvideo/exercicios/des03.py
@@ -0,0 +1,7 @@
+print ('=======DESAFIO 03=======')
+n1 = input ('Primeiro número:')
+n2 = input ('Segundo número:')
+n1 = int(n1)
+n2 = int(n2)
+soma = n1 + n2
+print ('A soma é:', soma)
---
+++ cursoemvideo/exercicios/teste01.py
@@ -0,0 +1,4 @@
+nome = input ('Qual o seu nome?')
+idade = input ('Qual a sua idade?')
+peso = input ('Qualo seu peso?')
+print (nome, idade, peso)
